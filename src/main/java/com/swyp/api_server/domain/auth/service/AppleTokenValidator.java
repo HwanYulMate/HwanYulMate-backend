95968d7a32c46c054a4fc1d21d55e4cd5ee8064d
전우선 <jeonwooseon@naver.com>
2025-09-17 03:38:06 +0900

    fix: @Value 어노테이션 수정

--- src/main/java/com/swyp/api_server/domain/auth/service/AppleTokenValidator.java
+++ src/main/java/com/swyp/api_server/domain/auth/service/AppleTokenValidator.java
@@ -34,7 +34,7 @@ public class AppleTokenValidator {
     private final CommonHttpClient httpClient;
     private final ObjectMapper objectMapper;
     
-    @Value("${spring.security.oauth2.client.registration.apple.client-id}")
+    @Value("${spring.security.oauth2.client.registration.apple.client-id:com.apptive.HwanYulMate}")
     private String appleClientId;
     
     private static final String APPLE_PUBLIC_KEYS_URL = "https://appleid.apple.com/auth/keys";
